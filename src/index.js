89f2217e82f1f5131f1c94cfe6816c99ce333ce3
Alexander <sanya55bychyk@gmail.com>
2021-03-06 11:34:27 +0300

    second commit

--- src/index.js
+++ src/index.js
@@ -1,18 +1,14 @@
-// import _ from 'lodash';
-
-// function component() {
-//     const element = document.createElement('div');
-
-//     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-//     return element;
-// }
-
-// document.body.appendChild(component());
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const temp = <h1>Hello there!</h1>; // variant to check babel
-// const temp = React.createElement('h1', {className: 'title'}, 'Hello there!'); //right variant
-ReactDOM.render(temp, document.getElementById('root'));
+class WebPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello World!</h1>
+      </div>
+    );
+  }
+};
+
+ReactDOM.render(<WebPage />, document.getElementById('root'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,16 @@
 const path = require('path');
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  module: {
-    rules: [
-      {
+module.exports = (env) => {
+  const isProduction = env === 'production';
+  return {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+      filename: 'main.js',
+      path: path.join(__dirname, 'dist'),
+    },
+    module: {
+      rules: [{
         test: /\.m?js$/,
         exclude: /node_modules/,
         use: {
@@ -17,7 +19,11 @@ module.exports = {
             presets: ['@babel/preset-env'],
           },
         },
-      },
-    ],
-  },
+      }],
+    },
+    devtool: isProduction ? 'source-map' : 'eval-cheap-module-source-map',
+    devServer: {
+      contentBase: path.join(__dirname, 'dist'),
+    },
+  };
 };
