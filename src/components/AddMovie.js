64a6dd8b062ec4f360ac3d4e457cc2bc66661ee5
Alexander <sanya55bychyk@gmail.com>
2021-04-25 22:55:14 +0300

    first commit

--- src/components/AddMovie.js
+++ src/components/AddMovie.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+import AddMovieModal from './AddMovieModal';
+
 const addMovieButton = '+ ADD MOVIE'; 
 
 export default class AddMovie extends Component {
@@ -7,6 +9,7 @@ export default class AddMovie extends Component {
     return (
       <div>
         <button className="addMovieButton">{addMovieButton}</button>
+        <AddMovieModal />
       </div>
     );
   }
---
+++ src/components/AddMovieModal.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Modal from 'react-modal';
+
+const AddMovieModal = (props) => (
+  <Modal
+    isOpen={true}
+    contentLabel="Add Movie"
+  >
+    <h1>Add Movie</h1>
+  </Modal>
+);
+
+export default AddMovieModal;
