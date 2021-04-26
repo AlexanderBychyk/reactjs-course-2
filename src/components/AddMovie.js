7f8adb13878f19a027b6178a7a3049a0da0539a1
Alexander <sanya55bychyk@gmail.com>
2021-04-26 19:12:16 +0300

    added addMovieMount

--- src/components/AddMovie.js
+++ src/components/AddMovie.js
@@ -5,11 +5,21 @@ import AddMovieModal from './AddMovieModal';
 const addMovieButton = '+ ADD MOVIE'; 
 
 export default class AddMovie extends Component {
+  state = {
+    showAddMovieModal: false,
+  };
+
+  handleOpenModal = () => {
+    this.setState(() => ({showAddMovieModal: true}));
+  }
+  handleCloseModal = () => {
+    this.setState(() => ({showAddMovieModal: false}));
+  }
   render () {
     return (
       <div>
-        <button className="addMovieButton">{addMovieButton}</button>
-        <AddMovieModal />
+        <button className="addMovieButton" onClick={this.handleOpenModal}>{addMovieButton}</button>
+        <AddMovieModal showAddMovieModal={this.state.showAddMovieModal} handleCloseModal={this.handleCloseModal} />
       </div>
     );
   }
--- src/components/AddMovieModal.js
+++ src/components/AddMovieModal.js
@@ -3,10 +3,26 @@ import Modal from 'react-modal';
 
 const AddMovieModal = (props) => (
   <Modal
-    isOpen={true}
+    isOpen={props.showAddMovieModal}
+    onRequestClose={props.handleCloseModal}
     contentLabel="Add Movie"
+    className="addMovieBackground"
+    overlayClassName="addMovieOverlay"
+    ariaHideApp={false}
   >
-    <h1>Add Movie</h1>
+    <div className="AddMovieModalBox">
+      <div>
+        <h1>Add Movie</h1>
+        <button onClick={props.handleCloseModal}>X</button>
+      </div>
+      <p>Moive name</p>
+      <input />
+      <p>Moive year</p>
+      <input />
+      <p>Moive ganre</p>
+      <input />
+      <button>Submit</button>
+    </div>
   </Modal>
 );
 
