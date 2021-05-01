c5a6d50745cca19c76d430c6ea3a87d18c6b4ac0
Alexander <sanya55bychyk@gmail.com>
2021-05-01 21:14:29 +0300

    work on AddMovieMount #2

--- src/components/AddMovieModal.js
+++ src/components/AddMovieModal.js
@@ -11,20 +11,44 @@ const AddMovieModal = (props) => (
     ariaHideApp={false}
   >
     <div className="AddMovieModalBox">
-      <button className="closeAddMovieModalButton" onClick={props.handleCloseModal}>X</button>
-      <h1>ADD MOVIE</h1>
-      <p>TITLE</p>
-      <input />
-      <p>RELEASE DATE</p>
-      <input />
-      <p>MOVIE URL</p>
-      <input />
-      <p>GANRE</p>
-      <input />
-      <p>OVERVIEW</p>
-      <input />
-      <p>RUNTIME</p>
-      <input />
+      {/* <button className="closeAddMovieModalButton" onClick={props.handleCloseModal}>X</button>
+      <h1>ADD MOVIE</h1> */}
+      <h2 className="AddMovieModalLabel">TITLE</h2>
+      <input className="AddMovieModalInput" 
+             type="input"
+             placeholder="Title here"
+      
+      />
+      <h2 className="AddMovieModalLabel">RELEASE DATE</h2>
+      <input className="AddMovieModalInput" 
+             type="input"
+             placeholder="Title here"
+      
+      />
+      <h2 className="AddMovieModalLabel">MOVIE URL</h2>
+      <input className="AddMovieModalInput" 
+             type="input"
+             placeholder="Title here"
+      
+      />
+      <h2 className="AddMovieModalLabel">GANRE</h2>
+      <input className="AddMovieModalInput" 
+             type="input"
+             placeholder="Title here"
+      
+      />
+      <h2 className="AddMovieModalLabel">OVERVIEW</h2>
+      <input className="AddMovieModalInput" 
+             type="input"
+             placeholder="Title here"
+      
+      />
+      <h2 className="AddMovieModalLabel">RUNTIME</h2>
+      <input className="AddMovieModalInput" 
+             type="input"
+             placeholder="Title here"
+      
+      />
       <div>  
         <button>Reset</button>
         <button>Submit</button>
